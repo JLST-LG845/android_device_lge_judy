5ef13c7ce3667e53c0abaa337e656686d73d678f
Tuan Anh <tuan73176@gmail.com>
2024-02-10 21:42:16 +0700

    judyln: Inherit from new makefile path(s)
    * Keep in mind that I don't want to miss any of them.

--- twrp_judyln.mk
+++ twrp_judyln.mk
@@ -17,13 +17,6 @@
 # Release name
 PRODUCT_RELEASE_NAME := judyln
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-#$(call inherit-product, build/target/product/embedded.mk)
-
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/twrp/config/common.mk)
-
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/judyln/device.mk)
 
